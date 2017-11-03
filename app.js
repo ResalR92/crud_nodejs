a493126ce65aaa09b45e08343a35f304da478aeb
Resal Ramdahadi <resalramdahadi92@gmail.com>
2017-11-03 14:25:54 +0700

    express middleware -> app.use((req,res,next)=>{req.name = \'test \'; next(); });

--- app.js
+++ app.js
@@ -2,8 +2,16 @@ const express = require('express');
 
 const app = express();
 
+// How middleware works
+app.use((req, res, next) => {
+  // console.log(Date.now());
+  req.name = 'Resal Ramdahadi';
+  next();
+});
+
 // Index Route
 app.get('/', (req, res) => {
+  console.log(req.name);
   res.send('INDEX');
 });
 
