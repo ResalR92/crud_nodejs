e8d02ef5efc057df9f6d87aa0bf7b23dbe001c32
Resal Ramdahadi <resalramdahadi92@gmail.com>
2017-11-03 16:45:43 +0700

    fetching from mongoDB -> Idea.find({}).sort({date:'desc'}).then(ideas => { res.render('ideas/index',{ideas:ideas});});

--- app.js
+++ app.js
@@ -51,6 +51,17 @@ app.get('/about', (req, res) => {
   res.render('about');
 });
 
+// Idea Index Page
+app.get('/ideas', (req, res) => {
+  Idea.find({})
+    .sort({date:'desc'})
+    .then(ideas => {
+      res.render('ideas/index', {
+        ideas:ideas
+      });
+    });
+});
+
 // Add idea form
 app.get("/ideas/add", (req, res) => {
   res.render('ideas/add');
