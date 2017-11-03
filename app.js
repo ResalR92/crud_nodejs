ed61f6853f14cab60d2a757974a297ee31532ecf
Resal Ramdahadi <resalramdahadi92@gmail.com>
2017-11-03 17:40:41 +0700

    delete data -> Idea.remove({_id:req.params.id}).then(()=>{res.redirect('/ideas');});

--- app.js
+++ app.js
@@ -135,6 +135,17 @@ app.put('/ideas/:id', (req,res) => {
   });
 });
 
+// Delete Idea
+app.delete('/ideas/:id', (req,res) => {
+  // res.send('DELETE');
+  Idea.remove({
+    _id : req.params.id
+  })
+  .then(() => {
+    res.redirect('/ideas');
+  });
+})
+
 const port = 5000;
 
 app.listen(port, () => {
