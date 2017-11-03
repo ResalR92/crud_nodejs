9457d325b0996dabcc95d9eb0c28ce6348d97a21
Resal Ramdahadi <resalramdahadi92@gmail.com>
2017-11-03 17:51:09 +0700

    npm install express-session connect-flash

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 const methodOverride = require("method-override"); //update
+const flash = require('connect-flash');
+const session = require('express-session');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose'); //mongoose odm
 
@@ -33,6 +35,28 @@ app.use(bodyParser.json());
 // Method override middleware
 // override with POST having ?_method=DELETE
 app.use(methodOverride('_method'));
+
+// Express Session middleware
+app.use(
+  session({
+    secret: "secret",
+    resave: true,
+    saveUninitialized: true,
+    cookie: { secure: true }
+  })
+);
+
+// Connect Flash
+app.use(flash());
+
+// Global variable
+app.use(function(req,res,next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+
+  next();
+});
 
 // How middleware works
 app.use((req, res, next) => {
