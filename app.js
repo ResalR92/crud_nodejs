9fb3163a3a1a3cd99b6455391c73b1f3b553cfd7
Resal Ramdahadi <resalramdahadi92@gmail.com>
2017-11-04 23:34:14 +0700

    global variable -> session -> req.user

--- app.js
+++ app.js
@@ -65,6 +65,7 @@ app.use(function(req,res,next) {
   res.locals.success_msg = req.flash('success_msg');
   res.locals.error_msg = req.flash('error_msg');
   res.locals.error = req.flash('error'); // for passport
+  res.locals.user = req.user || null;
 
   next();
 });
